16ae78c1078695bddf610a2b9ffa39089c34311b
RightHang <107296039+RightHang@users.noreply.github.com>
2023-10-26 11:29:26 +0800

    Update index.cjs
    V4.5

--- config/index.cjs
+++ config/index.cjs
@@ -114,7 +114,7 @@ const USER_CONFIG = {
       id: '72fd7e40bd0c4b2299a6742c2a5d3f84',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'pgZSiO1OhmJGgJOUEtoBcy8MLFT1v6X5iD9a0i6NA0Q',
+      useTemplateId: '0001',
       // 所在省份或城市，也可以不填
       province: '广东',
       // 所在城市或县区
